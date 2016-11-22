b1109aa925b6c4506426e435899f36e91b6b306b
Smurfffy <alanmurphy1995@gmail.com>
2016-11-22 12:29:15 +0000

    Created Azure App

--- HabitTracker_Windows_CS/HabitTracker/Properties/AssemblyInfo.cs
+++ HabitTracker_Windows_CS/HabitTracker/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MobileAppsProject")]
+[assembly: AssemblyTitle("HabitTracker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MobileAppsProject")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("HabitTracker")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
