b83a724c9d0579310c204fefca7173043c8b73ad
Smurfffy <alanmurphy1995@gmail.com>
2016-12-02 19:56:29 +0000

    Overhauld app, basic idea achived but must be heavily expanded on.

--- HabitHero/HabitHero/MainPage.xaml.cs
+++ HabitHero/HabitHero/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HabitHero.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -22,9 +23,33 @@ namespace HabitHero
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private MainPageViewModel _mainPageViewModel;
         public MainPage()
         {
             this.InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            habitFormControl.onHabitSaved += HabitFormControl_onHabitSaved;
+
+            if (_mainPageViewModel == null)
+            {
+                _mainPageViewModel = new MainPageViewModel();
+                DataContext = _mainPageViewModel;
+            }
+        }
+
+        private void HabitFormControl_onHabitSaved(object sender, Model.Habit e)
+        {
+            _mainPageViewModel.addNewHabit(e);
+        }
+
+        private void addHabitButton_Click(object sender, RoutedEventArgs e)
+        {
+            //opens the form for adding habits
+            habitFormControl.Visibility = Visibility.Visible;
         }
     }
 }
---
+++ HabitHero/HabitHero/Model/Habit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitHero.Model
+{
+    public class Habit
+    {
+        //This is or object for the habits
+        public string Name { get; set; }
+        public string Info { get; set; }
+    }
+}
