e7fba3fce63d5ee5b0bad09b0171e52ba7b92799
Smurfffy <alanmurphy1995@gmail.com>
2016-12-03 23:37:27 +0000

    Fixed Layout

--- Attempt one/UWPhabitHero/MainPage.xaml.cs
+++ Attempt one/UWPhabitHero/MainPage.xaml.cs
@@ -10,6 +10,7 @@
 using Microsoft.WindowsAzure.MobileServices;
 using System;
 using System.Threading.Tasks;
+using UWPhabitHero.ViewModel;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -24,6 +25,7 @@ namespace UWPhabitHero
 {
     public sealed partial class MainPage : Page
     {
+        private MainPageViewModel _mainPageViewModel;
         private MobileServiceCollection<TodoItem, TodoItem> items;
 #if OFFLINE_SYNC_ENABLED
         private IMobileServiceSyncTable<TodoItem> todoTable = App.MobileService.GetSyncTable<TodoItem>(); // offline sync
@@ -34,6 +36,26 @@ namespace UWPhabitHero
         public MainPage()
         {
             this.InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            HabitControl.OnHabitSaved += HabitControl_OnHabitSaved;  
+
+            if(_mainPageViewModel == null)
+            {
+                _mainPageViewModel = new MainPageViewModel();
+
+                DataContext = _mainPageViewModel;
+            }
+
+
+        }
+
+        private void HabitControl_OnHabitSaved(object sender, TodoItem e)
+        {
+            _mainPageViewModel.AddNewHAbit(e);
         }
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
@@ -110,9 +132,10 @@ namespace UWPhabitHero
 
         private async void ButtonSave_Click(object sender, RoutedEventArgs e)
         {
-            var todoItem = new TodoItem { Text = TextInput.Text };
+            /*var todoItem = new TodoItem { Text = TextInput.Text };
             TextInput.Text = "";
-            await InsertTodoItem(todoItem);
+            await InsertTodoItem(todoItem);*/
+            HabitControl.Visibility = Visibility.Visible;
         }
 
         private async void CheckBoxComplete_Checked(object sender, RoutedEventArgs e)
