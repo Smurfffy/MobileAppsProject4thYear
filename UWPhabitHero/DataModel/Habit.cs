0eb8344cf3cdac5248ec14851e19cce131b12dc3
Smurfffy <alanmurphy1995@gmail.com>
2016-12-04 01:52:59 +0000

    Data now adds to object

---
+++ UWPhabitHero/DataModel/Habit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPhabitHero.DataModel
+{
+    public class Habit
+    {
+        public string Name { get; set; }
+
+        public int Rating { get; set; }
+
+        public string Reason { get; set; }
+    }
+}
--- UWPhabitHero/UserControls/AddHabitControl.xaml.cs
+++ UWPhabitHero/UserControls/AddHabitControl.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using UWPhabitHero.DataModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -19,9 +20,44 @@ namespace UWPhabitHero.UserControls
 {
     public sealed partial class AddHabitControl : UserControl
     {
+        public event EventHandler<Habit> OnHabitSaved;
+
         public AddHabitControl()
         {
             this.InitializeComponent();
+        }
+
+        private void SaveHabitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //fire event for data
+            var newHabit = new Habit();
+            newHabit.Name = HabitNameTxtBox.Text;
+            newHabit.Rating = Convert.ToInt32(RatingTxtBox.Text);
+            newHabit.Reason = ReasonTxtBox.Text;
+
+            FireOnHabitSaved(newHabit);
+            
+            ClearTxtBoxes();
+            Visibility = Visibility.Collapsed;
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Visibility = Visibility.Collapsed;
+            ClearTxtBoxes();
+        }
+
+        private void ClearTxtBoxes()
+        {
+            // Clears the text boxes
+            HabitNameTxtBox.Text = string.Empty;
+            RatingTxtBox.Text = string.Empty;
+            ReasonTxtBox.Text = string.Empty;
+        }
+
+        private void FireOnHabitSaved(Habit newHabit)
+        {
+            OnHabitSaved?.Invoke(null, newHabit);
         }
     }
 }
